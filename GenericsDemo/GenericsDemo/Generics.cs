04b134949d022e4317e494938072d7c46ac978b6
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-29 19:50:14 -0400

    [Ruchita] Add: Generics Demo

---
+++ GenericsDemo/GenericsDemo/Generics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+//Generics means general form.
+//It doesnot specify aprticular data type
+//We are using <> (Angular type) with type name.
+//TypeName<T> T is your type paramter.
+//Generics allow us to a single class or method that can be used in Different data type.
+//We can reuse the code.
+{ 
+    public class Generics<T>
+    {
+    //public string name;
+    public T data;
+
+    public Generics(T data)
+    {
+        this.data = data;
+        Console.WriteLine(this.data);
+    }
+
+        public void GenericMethod(int num)
+        {
+            Console.WriteLine(num);
+        }
+        
+    }
+    public class GenericsMethod
+    {
+        public void GenericMethodOne<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int> generics = new Generics<int>(25);
+            Generics<string> genericsString = new Generics<string>("Ruchita");
+            Generics<float> genericsfloat = new Generics<float>(25.23f);
+            Generics<double> genericsdouble = new Generics<double>(25.1234);
+            Generics<char> genericschar = new Generics<char>('R');
+            Generics<bool> genericsbool = new Generics<bool>(true);
+            Console.WriteLine("-------------------------------------");
+
+            //Generic method 
+            generics.GenericMethod(9);
+           genericsString.GenericMethod("Ruchi");
+            //genericsString.GenericMethod(9);
+            // genericsString.GenericMethod("Aabha");
+            // genericsDouble.GenericMethod(12.5);
+            // genericsChar.GenericMethod(false);
+            Console.WriteLine("-------------------------------------------");
+
+
+            GenericsMethod obj =new GenericsMethod();
+            obj.GenericMethodOne<int>(30);
+            obj.GenericMethodOne<string>("ABC");
+            obj.GenericMethodOne<float>(123.5f);
+            obj.GenericMethodOne<double>(2345.6);
+            obj.GenericMethodOne<bool>(false);
+            obj.GenericMethodOne<char>('A');
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
